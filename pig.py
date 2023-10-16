e4c67b9c8181a252ab5afe3da071fcf87b1c1650
anisularman420 <Anisul.Arman19@spsmail.cuny.edu>
2023-10-16 02:37:22 -0400

    In this script i can continue the game and also add multiple played to play game by using the argument --numGame and add multiple game by adding number of games with selecting the num of players

--- pig.py
+++ pig.py
@@ -1,61 +1,71 @@
-import argparse
 import random
-
+import sys
 class Player:
-    def __init__(self, number):
-        self.number = number
-        self.score = 0
+    def __init__(self, player_id):
+        self.player_id = player_id
+        self.total_score = 0
+        self.turn_score = 0
+
+# Two blank lines here
 
 def roll_die():
     return random.randint(1, 6)
 
-def play_game(num_players):
-    players = [Player(i + 1) for i in range(num_players)]
-    player_index = 0
 
+def take_turn(player):
+    print(f"Player {player.player_id}'s turn.")
     while True:
-        player = players[player_index]
-
-        print(f"Player {player.number}'s turn.")
-        turn_score = 0
-        while True:
-            decision = input(f"Roll (r) or Hold (h): ").lower()
-            if decision == 'r':
-                roll = roll_die()
-                print(f"Player {player.number} rolled a {roll}.")
-                if roll == 1:
-                    print(f"Player {player.number} rolled a 1. Turn score reset.")
-                    turn_score = 0
-                    break
-                else:
-                    turn_score += roll
-                    print(f"Player {player.number}'s turn score: {turn_score}")
-            elif decision == 'h':
-                player.score += turn_score
-                print(f"Player {player.number} chose to hold.")
-                print(f"Player {player.number}'s total score: {player.score}")
-                if player.score >= 100:
-                    print(f"Player {player.number} wins with a score of {player.score}!")
-                    return
+        player_choice = input("Roll (r) or Hold (h): ").lower()
+        if player_choice == 'r':
+            roll = roll_die()
+            if roll == 1:
+                print(f"Player {player.player_id} rolled a 1. Turn score reset.")
+                player.turn_score = 0
                 break
             else:
-                print("Invalid input. Please enter 'r' to roll or 'h' to hold.")
+                player.turn_score += roll
+                print(f"Player {player.player_id} rolled a {roll}.")
+                print(f"Player {player.player_id}'s turn score: {player.turn_score}")
+        elif player_choice == 'h':
+            player.total_score += player.turn_score
+            player.turn_score = 0
+            print(f"Player {player.player_id} chose to hold.")
+            print(f"Player {player.player_id}'s total score: {player.total_score}")
+            break
+        else:
+            print("Invalid input. Please enter 'r' to roll or 'h' to hold.")
 
-        player_index = (player_index + 1) % num_players
+def play_game(num_players, players):
+    current_player_index = 0
+
+    while True:
+        current_player = players[current_player_index]
+        take_turn(current_player)
+
+        if current_player.total_score >= 100:
+            print(f"Player {current_player.player_id} wins with a score of {current_player.total_score}!")
+            break
+
+        current_player_index = (current_player_index + 1) % num_players
+
+    # Reset game state after the game ends
+    for player in players:
+        player.total_score = 0
+        player.turn_score = 0
 
 def main():
-    parser = argparse.ArgumentParser()
-    parser.add_argument('--numPlayers', type=int, default=2, help='Number of players in the game')
-    parser.add_argument('--numGames', type=int, default=1, help='Number of games to play')
-    args = parser.parse_args()
-
-    num_players = args.numPlayers
-    num_games = args.numGames
+    num_players = int(input("Enter the number of players: "))
+    num_games = int(input("Enter the number of games: "))
 
     for game in range(num_games):
         print(f"Game {game + 1}")
-        play_game(num_players)
-        print("End of Game")
+        players = [Player(i) for i in range(1, num_players + 1)]
+        play_game(num_players, players)
+        # Reset game state
+        print("Game over. Resetting scores and game state.")
+        for player in players:
+            player.total_score = 0
+            player.turn_score = 0
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
